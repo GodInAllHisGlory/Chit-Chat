a270272f2a3400bcd52d890f2f0912769e8564a5
Nathan <nathanjohnhaight@gmail.com>
2025-12-09 21:56:03 -0700

    You can talk to others

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,32 +3,76 @@ import { useState, useEffect, useContext } from 'react'
 import Message from "./Message.jsx"
 import * as cookie from "cookie";
 
-const id = context.user;
+const user = {
+    user: context.user,
+    chatId: "",
+    partner: ""
+}
+const userJSON = JSON.stringify(user);
+
 
 function Chat() {
-    const chatSocket = new WebSocket( //Makes a websocket to talk to the other person
-    'ws://'
-    + window.location.host
-    + '/chat/'
-    + id
-    + '/'
-);
+    const [isReady, setIsReady] = useState(false);
+    const [socket, setSocket] = useState(null);
 
-useEffect(() => {
-    const res = fetch("chat/queue_chatter", {
-            method: "post",
-            credentials: "same-origin",
-            body: 
-                context.user,
-            headers:{
-                "Content-Type": "application/json",
-                "X-CSRFToken": cookie.parse(document.cookie).csrftoken,
+    useEffect( () => { 
+        (async () => {
+            async function queueUser(){
+                const res = fetch("chat/queue_chatter", {
+                    method: "post",
+                    credentials: "same-origin",
+                    body: 
+                        userJSON,
+                        headers:{
+                            "Content-Type": "application/json",
+                            "X-CSRFToken": cookie.parse(document.cookie).csrftoken,
+                        }
+                    }
+                )
             }
-})},[])
-
+            await queueUser()
+            
+            async function matchMake() {
+                while(user.chatId === ""){
+                    console.log("Working");
+                    const res = await fetch("chat/match_maker", {
+                        method: "post",
+                        credentials: "same-origin",
+                        body: 
+                            userJSON,
+                            headers:{
+                                "Content-Type": "application/json",
+                                "X-CSRFToken": cookie.parse(document.cookie).csrftoken,
+                            }
+                        }
+                    ).then(r => r.json());
+                    user.chatId = res.chatId
+                    user.partner = res.partner
+                    console.log(res);
+                    
+                }
+            }
+            await matchMake() 
+            setSocket(new WebSocket( //Makes a websocket to talk to the other person
+            `ws://${window.location.host}/chat/${user.chatId}/`)); 
+            setIsReady(true);
+            console.log(socket);
+        })()
+    },[]);
+    console.log(socket);
+    function messageRender(){
+        if(isReady){
+            return(
+                <div>
+                    <h2>Now talking to {user.partner}</h2>
+                    <Message chatSocket={socket}/>
+                </div>
+            )
+        }
+    }
     return(
         <div>
-            <Message chatSocket={chatSocket}/>
+            {messageRender()}
         </div>
     )
 }
