dade383cb93b617d2a957e9effbe0a8c53515892
Nathan <nathanjohnhaight@gmail.com>
2025-12-09 21:55:39 -0700

    You can now match make with others

--- _server/core/urls.py
+++ _server/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', view=views.index, name="index"),
     path('send_message', view=views.send_message, name="send_message"),
     path('chat/queue_chatter', view=views.queue_chatter, name="queue_chatter"),
+    path('chat/match_maker',view=views.match_maker, name="mathch_maker"),
 ]
--- _server/core/views.py
+++ _server/core/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.conf  import settings
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 import json
 import os
-from django.contrib.auth.decorators import login_required
+import string
+import random
+import time
 
 # Load manifest when server launches
 MANIFEST = {}
 chatter_queue = []
+open_rooms = []
+
 if not settings.DEBUG:
     f = open(f"{settings.BASE_DIR}/core/static/manifest.json")
     MANIFEST = json.load(f)
@@ -34,5 +39,41 @@ def send_message(req):
 @login_required
 def queue_chatter(req):
     chatter = json.loads(req.body)
-    chatter_queue.append(chatter)
-    print(chatter)
+    if chatter not in chatter_queue:
+        chatter_queue.append(chatter)
+    return JsonResponse(chatter)
+
+@login_required
+def match_maker(req):
+    user = json.loads(req.body)
+
+    for chatter in chatter_queue:
+        if chatter['user'] == user['user']:
+            userIndex = chatter_queue.index(chatter) #Make a refrence to where this element is so we can remove it when we find a partner
+            if chatter['chatId'] != "":
+                chatter_queue.remove(chatter)
+                return JsonResponse(chatter) 
+            else:
+                break
+                
+
+    chat_id = make_id()
+
+    for chatter in chatter_queue:
+        if chatter['user'] != user['user'] and chatter['chatId'] == "":
+            chatter['chatId'] = chat_id
+            chatter['partner'] = user['user']
+            user['chatId'] = chat_id
+            user['partner'] = chatter['user']
+            print(userIndex);
+            del chatter_queue[userIndex]
+            break
+    print(f"\090[92m{chatter_queue}\033[0m")
+    return JsonResponse(user) 
+    
+def make_id():
+    full_string_set = string.ascii_letters + string.digits
+    id = "".join(random.choice(full_string_set) for _ in range(8))
+    return id
+
+
